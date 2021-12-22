e4a25ed91bf43bea1e05dbcfc2511fc4652a16c5
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2021-12-22 12:52:43 +0100

    Task-52442: Implement DeleteNewsTargets UpgradePlugin Prior to this change, news targets are already created without labels, thus we need to implement an upgrade plugin in order to delete the old created news targets and allow creating the right one with label and referenced properties.

---
+++ data-upgrade-news/src/test/java/org/exoplatform/news/upgrade/targets/DeleteNewsTargetsUpgradePluginTest.java
@@ -0,0 +1,55 @@
+package org.exoplatform.news.upgrade.targets;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.container.xml.ValueParam;
+import org.exoplatform.news.rest.NewsTargetingEntity;
+import org.exoplatform.news.service.NewsTargetingService;
+
+@RunWith(PowerMockRunner.class)
+public class DeleteNewsTargetsUpgradePluginTest {
+
+  @Mock
+  NewsTargetingService newsTargetingService;
+  
+  @Test
+  public void testNewsDeleteTargetsMigration() throws Exception {
+    InitParams initParams = new InitParams();
+
+    ValueParam valueParam = new ValueParam();
+    valueParam.setName("product.group.id");
+    valueParam.setValue("org.exoplatform.addons.platform");
+    initParams.addParameter(valueParam);
+    
+    NewsTargetingEntity newsTarget1 = new NewsTargetingEntity();
+    newsTarget1.setName("target1");
+    newsTarget1.setName("target 1");
+    
+    NewsTargetingEntity newsTarget2 = new NewsTargetingEntity();
+    newsTarget1.setName("target2");
+    newsTarget1.setName("target 2");
+    
+    List<NewsTargetingEntity> newsTargets = new ArrayList<>();
+    newsTargets.add(newsTarget1);
+    newsTargets.add(newsTarget2);
+
+    when(newsTargetingService.getTargets()).thenReturn(newsTargets);
+    DeleteNewsTargetsUpgradePlugin deleteNewsTargetsUpgradePlugin = new DeleteNewsTargetsUpgradePlugin(initParams, newsTargetingService);
+    deleteNewsTargetsUpgradePlugin.processUpgrade(null, null);
+    verify(newsTargetingService, times(2)).deleteTargetByName(any());
+    assertEquals(2, deleteNewsTargetsUpgradePlugin.getNewsTargetsCount());
+  }
+}
