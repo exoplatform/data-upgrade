11725703f653248565a8761e2dc913358cf39cb2
sofyenne <shajchedli@exoplatform.com>
2023-01-06 16:51:51 +0100

    add header

--- data-upgrade-app-registry/src/main/java/org/exoplatform/application/upgrade/AppRegistryUpgradeToolsCategoryPlugin.java
+++ data-upgrade-app-registry/src/main/java/org/exoplatform/application/upgrade/AppRegistryUpgradeToolsCategoryPlugin.java
@@ -1,6 +1,28 @@
+/*
+ * Copyright (C) 2022 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <gnu.org/licenses>.
+ */
 package org.exoplatform.application.upgrade;
 
+import java.math.BigInteger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import org.apache.commons.lang3.StringUtils;
+
 import org.exoplatform.commons.persistence.impl.EntityManagerService;
 import org.exoplatform.commons.upgrade.UpgradePluginExecutionContext;
 import org.exoplatform.commons.upgrade.UpgradeProductPlugin;
@@ -10,10 +32,6 @@ import org.exoplatform.container.component.RequestLifeCycle;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import java.math.BigInteger;
 
 public class AppRegistryUpgradeToolsCategoryPlugin extends UpgradeProductPlugin {
 
