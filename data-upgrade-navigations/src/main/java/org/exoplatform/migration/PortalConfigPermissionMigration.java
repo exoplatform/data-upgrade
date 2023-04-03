cf026d24c20466077cc114b5daa48b0c0b386652
Romain Dénarié <romain.denarie@exoplatform.com>
2023-04-03 16:01:35 +0200

    add action to clear navigation cache

--- data-upgrade-navigations/src/main/java/org/exoplatform/migration/PortalConfigPermissionMigration.java
+++ data-upgrade-navigations/src/main/java/org/exoplatform/migration/PortalConfigPermissionMigration.java
@@ -8,27 +8,39 @@ import org.exoplatform.container.component.RequestLifeCycle;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.portal.jdbc.entity.PermissionEntity;
 import org.exoplatform.portal.jdbc.entity.SiteEntity;
+import org.exoplatform.portal.mop.storage.cache.CacheDescriptionStorage;
+import org.exoplatform.portal.mop.storage.cache.CacheNavigationStorage;
+import org.exoplatform.services.cache.CacheService;
+import org.exoplatform.services.cache.ExoCache;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.io.Serializable;
 import java.util.List;
 
 public class PortalConfigPermissionMigration  extends UpgradeProductPlugin {
     private final PortalContainer      container;
 
     private final EntityManagerService entityManagerService;
+
+    int pagesNodesCount;
+
+    private CacheService cacheService;
+
     private static final Log LOG                        = ExoLogger.getExoLogger(PortalConfigPermissionMigration.class);
 
     public PortalConfigPermissionMigration(PortalContainer container,
                                     EntityManagerService entityManagerService,
+                                    CacheService cacheService,
                                     InitParams initParams) {
         super(initParams);
         this.container = container;
         this.entityManagerService = entityManagerService;
-
+        this.cacheService = cacheService;
+        pagesNodesCount=0;
     }
     @Override
     public void processUpgrade(String s, String s1) {
@@ -64,7 +76,7 @@ public class PortalConfigPermissionMigration  extends UpgradeProductPlugin {
                 updateNativeQuery.setParameter("permission",permission);
                 updateNativeQuery.setParameter("permissionId",permissionEntity.getId());
 
-                updateNativeQuery.executeUpdate();
+                this.pagesNodesCount += updateNativeQuery.executeUpdate();
                 LOG.info("Update permission from {} to {} for portalConfig {}",permissionEntity.getPermission(), permission, name);
 
             });
@@ -76,6 +88,7 @@ public class PortalConfigPermissionMigration  extends UpgradeProductPlugin {
             if (transactionStarted && entityManager.getTransaction().isActive()) {
                 entityManager.getTransaction().commit();
             }
+            clearNavigationCache();
 
         } catch (Exception e) {
             if (transactionStarted && entityManager.getTransaction().isActive() && entityManager.getTransaction().getRollbackOnly()) {
@@ -84,6 +97,26 @@ public class PortalConfigPermissionMigration  extends UpgradeProductPlugin {
             throw new RuntimeException("Unable to update PortalSite",e);
         } finally {
             RequestLifeCycle.end();
+        }
+    }
+    private void clearNavigationCache() {
+        if (pagesNodesCount > 0) {
+            ExoCache<? extends Serializable, ?> navigationCache = this.cacheService.getAllCacheInstances()
+                                                                                   .stream()
+                                                                                   .filter(cache -> CacheNavigationStorage.NAVIGATION_CACHE_NAME.equals(cache.getName()))
+                                                                                   .findFirst()
+                                                                                   .orElse(null);
+            if (navigationCache != null) {
+                navigationCache.clearCache();
+            }
+            ExoCache<? extends Serializable, ?> descriptionCache = this.cacheService.getAllCacheInstances()
+                                                                                    .stream()
+                                                                                    .filter(cache -> CacheDescriptionStorage.DESCRIPTION_CACHE_NAME.equals(cache.getName()))
+                                                                                    .findFirst()
+                                                                                    .orElse(null);
+            if (descriptionCache != null) {
+                descriptionCache.clearCache();
+            }
         }
     }
 }
