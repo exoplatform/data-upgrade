4b26cec591b7c847fe374c5ad77ac848910db84e
sofyenne <shajchedli@exoplatform.com>
2024-03-07 22:10:24 +0100

    Update the file header

--- data-upgrade-navigations/src/main/java/org/exoplatform/migration/PortalNavigationIconMigration.java
+++ data-upgrade-navigations/src/main/java/org/exoplatform/migration/PortalNavigationIconMigration.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2003-2024 eXo Platform SAS
+ * Copyright (C) 2024 eXo Platform SAS.
  *
- *  This program is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -9,10 +9,10 @@
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <gnu.org/licenses>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.exoplatform.migration;
 
