73908038aa8a0a21217ecd9967079d1917bd43fb
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2024-03-08 15:47:46 +0100

    fix: Configure the PortalNavigationIconUpgradePlugin to set icons to the portal navigation nodes - EXO-69848 (#210)
    Prior to this change after migrating from the 6.4 to the 6.5 then to the 6.6 version , the external stream node navigation icon was incorrect , this change is going to configure the PortalNavigationIconUpgradePlugin in order to set icons for the portal navigation nodes .

---
+++ data-upgrade-navigations/src/test/java/org/exoplatform/migration/PortalNavigationIconMigrationTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2024 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.migration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.exoplatform.commons.persistence.impl.EntityManagerService;
+import org.exoplatform.component.test.AbstractKernelTest;
+import org.exoplatform.component.test.ConfigurationUnit;
+import org.exoplatform.component.test.ConfiguredBy;
+import org.exoplatform.component.test.ContainerScope;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.container.component.RequestLifeCycle;
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.container.xml.ValueParam;
+import org.exoplatform.portal.jdbc.entity.NodeEntity;
+import org.exoplatform.portal.jdbc.entity.PageEntity;
+import org.exoplatform.portal.jdbc.entity.SiteEntity;
+import org.exoplatform.portal.mop.NodeTarget;
+import org.exoplatform.portal.mop.PageType;
+import org.exoplatform.portal.mop.SiteType;
+import org.exoplatform.portal.mop.dao.NodeDAO;
+import org.exoplatform.portal.mop.dao.PageDAO;
+import org.exoplatform.portal.mop.dao.SiteDAO;
+
+@ConfiguredBy({ @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/portal/configuration.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.portal-configuration-local.xml"),
+    @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "org/exoplatform/portal/config/conf/configuration.xml") })
+public class PortalNavigationIconMigrationTest extends AbstractKernelTest {
+  InitParams                            initParams = new InitParams();
+  private PortalContainer               container;
+  private EntityManagerService          entityManagerService;
+  private PortalNavigationIconMigration portalNavigationIconMigration;
+  private SiteDAO                       siteDAO;
+  private PageDAO                       pageDAO;
+  private NodeDAO                       nodeDAO;
+
+  @Before
+  public void setUp() {
+    container = getContainer();
+    siteDAO = container.getComponentInstanceOfType(SiteDAO.class);
+    pageDAO = container.getComponentInstanceOfType(PageDAO.class);
+    nodeDAO = container.getComponentInstanceOfType(NodeDAO.class);
+    entityManagerService = container.getComponentInstanceOfType(EntityManagerService.class);
+    begin();
+    ValueParam productGroupIdValueParam = new ValueParam();
+    productGroupIdValueParam.setName("product.group.id");
+    productGroupIdValueParam.setValue("org.exoplatform.platform");
+    ValueParam portalNodeNamesValueParam = new ValueParam();
+    portalNodeNamesValueParam.setName("portal.node.names");
+    portalNodeNamesValueParam.setValue("external-stream");
+    ValueParam portalNodeIconsValueParam = new ValueParam();
+    portalNodeIconsValueParam.setName("portal.node.icons");
+    portalNodeIconsValueParam.setValue("fas fa-user-lock");
+    initParams.addParameter(productGroupIdValueParam);
+    initParams.addParameter(portalNodeNamesValueParam);
+    initParams.addParameter(portalNodeIconsValueParam);
+    this.portalNavigationIconMigration = new PortalNavigationIconMigration(entityManagerService, initParams);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    end();
+  }
+
+  @Test
+  public void testPortalNavigationIconMigration() {
+
+    SiteEntity siteEntity = new SiteEntity();
+    siteEntity.setName("dw");
+    siteEntity.setSiteType(SiteType.PORTAL);
+    siteDAO.create(siteEntity);
+    siteEntity = siteDAO.findByType(SiteType.PORTAL).stream().filter(e -> e.getName().equals("dw")).toList().get(0);
+    //
+    assertNotNull(siteEntity);
+    PageEntity pageEntity = new PageEntity();
+    pageEntity.setName("stream");
+    pageEntity.setOwner(siteEntity);
+    pageEntity.setPageType(PageType.PAGE);
+    pageDAO.create(pageEntity);
+    pageEntity = pageDAO.findAll().stream().filter(e -> e.getName().equals("stream")).toList().get(0);
+    //
+    assertNotNull(pageEntity);
+    NodeEntity nodeEntity = new NodeEntity();
+    nodeEntity.setName("external-stream");
+    nodeEntity.setIcon(null);
+    nodeEntity.setPage(pageEntity);
+    nodeEntity.setTarget(NodeTarget.NEW_TAB);
+    nodeDAO.create(nodeEntity);
+    nodeEntity = nodeDAO.findAllByPage(pageEntity.getId()).stream().filter(e -> e.getName().equals("external-stream")).toList().get(0);
+    //
+    assertNotNull(nodeEntity);
+    restartTransaction();
+    portalNavigationIconMigration.processUpgrade(null, null);
+    //
+    assertEquals(1, portalNavigationIconMigration.getMigratedPortalNodeIconsNodeIcons());
+    nodeEntity = nodeDAO.find(nodeEntity.getId());
+    assertNotNull(nodeEntity);
+    assertNotNull(nodeEntity.getIcon());
+    assertEquals("fas fa-user-lock", nodeEntity.getIcon());
+  }
+
+}
