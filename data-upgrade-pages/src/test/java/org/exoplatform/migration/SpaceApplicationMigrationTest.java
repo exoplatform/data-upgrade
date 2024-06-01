4af80358cc092da8730bbe408ebe3f3751fcb22f
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2024-06-01 01:16:10 +0100

    feat: Implement Portal pages profiles and configure page migration upgrade plugin to upgrade content pages - EXO-71740 - Meeds-io/MIPs#130 (#220)

--- data-upgrade-pages/src/test/java/org/exoplatform/migration/SpaceApplicationMigrationTest.java
+++ data-upgrade-pages/src/test/java/org/exoplatform/migration/SpaceApplicationMigrationTest.java
@@ -1,145 +1,134 @@
 package org.exoplatform.migration;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import org.exoplatform.commons.api.settings.SettingService;
-import org.exoplatform.commons.api.settings.SettingValue;
-import org.exoplatform.commons.api.settings.data.Context;
-import org.exoplatform.commons.api.settings.data.Scope;
 import org.exoplatform.commons.persistence.impl.EntityManagerService;
-import org.exoplatform.component.test.AbstractKernelTest;
-import org.exoplatform.component.test.ConfigurationUnit;
-import org.exoplatform.component.test.ConfiguredBy;
-import org.exoplatform.component.test.ContainerScope;
+import org.exoplatform.commons.utils.ListAccess;
 import org.exoplatform.container.PortalContainer;
-import org.exoplatform.container.component.RequestLifeCycle;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.container.xml.ValueParam;
-import org.exoplatform.portal.mop.navigation.NavigationContext;
-import org.exoplatform.portal.mop.service.NavigationService;
-import org.exoplatform.services.security.ConversationState;
-import org.exoplatform.services.security.Identity;
-import org.exoplatform.services.security.IdentityRegistry;
-import org.exoplatform.services.security.MembershipEntry;
-import org.exoplatform.social.core.space.*;
+import org.exoplatform.social.core.space.SpaceFilter;
 import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.space.spi.SpaceService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.MockitoJUnitRunner;
+import jakarta.persistence.Query;
 
-
-import java.util.HashSet;
-
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-@ConfiguredBy({
-  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/portal/configuration.xml"),
-  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/exo.portal.component.portal-configuration-local.xml"),
-  @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "org/exoplatform/portal/config/conf/configuration.xml")
-})
-public class SpaceApplicationMigrationTest extends AbstractKernelTest {
+public class SpaceApplicationMigrationTest {
 
-  protected PortalContainer         container;
+  @Mock
+  private SpaceService                               spaceService;
 
-  protected SpaceService            spaceService;
+  @Mock
+  private EntityManagerService                       entityManagerService;
 
-  protected NavigationService       navigationService;
+  @Mock
+  private SettingService                             settingService;
 
-  protected IdentityRegistry        identityRegistry;
+  @Mock
+  private PortalContainer                            rootContainer;
 
-  protected EntityManagerService    entityManagerService;
+  @Mock
+  private EntityManager                              entityManager;
 
-  protected SettingService          settingService;
+  private SpaceApplicationMigration                  spaceApplicationMigration;
 
-  private SpaceApplicationMigration spaceApplicationMigration;
-
-  public SpaceApplicationMigrationTest() {
-    setForceContainerReload(true);
-  }
+  private static final MockedStatic<PortalContainer> portalContainerMockedStatic = mockStatic(PortalContainer.class);
 
   @Before
   public void setUp() {
-    container = getContainer();
-    this.spaceService = container.getComponentInstanceOfType(SpaceService.class);
-    this.navigationService = container.getComponentInstanceOfType(NavigationService.class);
-    this.identityRegistry = container.getComponentInstanceOfType(IdentityRegistry.class);
-    this.entityManagerService = container.getComponentInstanceOfType(EntityManagerService.class);
-    this.settingService = container.getComponentInstanceOfType(SettingService.class);
-
-    RequestLifeCycle.begin(container);
+    // Initialize InitParams without mocking
     InitParams initParams = new InitParams();
     ValueParam valueParam = new ValueParam();
     valueParam.setName("product.group.id");
     valueParam.setValue("org.exoplatform.platform");
-    ValueParam oldAppNamevalueParam = new ValueParam();
-    oldAppNamevalueParam.setName("old.app.name");
-    oldAppNamevalueParam.setValue("Documents");
-    ValueParam oldAppIdvalueParam = new ValueParam();
-    oldAppIdvalueParam.setName("old.app.id");
-    oldAppIdvalueParam.setValue("FileExplorerPortlet");
-    ValueParam newAppIdvalueParam = new ValueParam();
-    newAppIdvalueParam.setName("new.app.id");
-    newAppIdvalueParam.setValue("Documents");
     initParams.addParameter(valueParam);
-    initParams.addParameter(oldAppNamevalueParam);
-    initParams.addParameter(oldAppIdvalueParam);
-    initParams.addParameter(newAppIdvalueParam);
 
-    spaceApplicationMigration = new SpaceApplicationMigration(spaceService, entityManagerService, settingService, initParams);
+    ValueParam oldAppNameParam = new ValueParam();
+    oldAppNameParam.setName("old.app.name");
+    oldAppNameParam.setValue("oldAppName");
+
+    ValueParam oldAppIdParam = new ValueParam();
+    oldAppIdParam.setName("old.app.id");
+    oldAppIdParam.setValue("oldAppId");
+
+    ValueParam newAppIdParam = new ValueParam();
+    newAppIdParam.setName("new.app.id");
+    newAppIdParam.setValue("newAppId");
+
+    initParams.addParam(oldAppNameParam);
+    initParams.addParam(oldAppIdParam);
+    initParams.addParam(newAppIdParam);
+
+    this.spaceApplicationMigration =
+                                   new SpaceApplicationMigration(spaceService, entityManagerService, settingService, initParams);
   }
 
   @After
-  public void tearDown() throws Exception {
-    RequestLifeCycle.end();
+  public void tearDown() {
+    if (!portalContainerMockedStatic.isClosed()) {
+      portalContainerMockedStatic.close();
+    }
   }
 
   @Test
-  public void processUpgrade() throws Exception {
-    startAdminSession();
+  public void testProcessUpgrade() throws Exception {
 
-    Space space = new Space();
-    space.setDisplayName("testspace");
-    space.setPrettyName(space.getDisplayName());
-    String shortName = SpaceUtils.cleanString(space.getDisplayName());
-    space.setGroupId("/spaces/" + shortName);
-    space.setUrl(shortName);
-    space.setEditor("root");
-    space.setRegistration("validation");
-    space.setTemplate("classic");
-    space.setVisibility("public");
-    space.setPriority("2");
-    String[] manager = new String[] { "root" };
-    String[] members = new String[] { "root", "john" };
-    space.setManagers(manager);
-    space.setMembers(members);
+    Space space = mock(Space.class);
+    when(space.getApp()).thenReturn("app");
+    Space[] spacesArray = new Space[] { space };
+    ListAccess<Space> listAccessMock = mock(ListAccess.class);
 
-    space = spaceService.createSpace(space, "root");
-    assertTrue(SpaceUtils.isInstalledApp(space, "FileExplorerPortlet"));
-    spaceApplicationMigration.processUpgrade(null, null);
-    space = spaceService.getSpaceById(space.getId());
-    assertFalse(SpaceUtils.isInstalledApp(space, "FileExplorerPortlet"));
-    assertTrue(SpaceUtils.isInstalledApp(space, "Documents"));
+    portalContainerMockedStatic.when(PortalContainer::getInstance).thenReturn(rootContainer);
+    when(spaceService.getAllSpacesByFilter(any(SpaceFilter.class))).thenReturn(listAccessMock);
+    when(listAccessMock.load(anyInt(), anyInt())).thenReturn(spacesArray);
+    when(listAccessMock.getSize()).thenReturn(spacesArray.length);
+    when(space.getGroupId()).thenReturn("groupId");
+    when(space.getId()).thenReturn("spaceId");
+    when(space.getDisplayName()).thenReturn("Space Display Name");
+
+    Query queryMock = mock(Query.class);
+    when(entityManagerService.getEntityManager()).thenReturn(entityManager);
+    EntityTransaction transaction = mock(EntityTransaction.class);
+    when(entityManager.getTransaction()).thenReturn(transaction);
+    when(transaction.isActive()).thenReturn(true);
+    when(entityManager.createNativeQuery(anyString())).thenReturn(queryMock);
+
+    spaceApplicationMigration.processUpgrade("newVersion", "previousGroupVersion");
+
+    // verify remove old application
+    verify(entityManager,
+           times(1)).createNativeQuery(eq("DELETE FROM PORTAL_PAGES WHERE SITE_ID IN (SELECT ID FROM PORTAL_SITES WHERE NAME ='groupId') AND NAME='oldAppId'"));
+    verify(entityManager, times(1)).createNativeQuery(eq("DELETE FROM SOC_APPS WHERE APP_ID='oldAppId' AND SPACE_ID= 'spaceId'"));
+    verify(queryMock, times(2)).executeUpdate();
+
+    // verify install new application
+    verify(spaceService, times(1)).installApplication(any(Space.class), eq("newAppId"));
+    verify(spaceService, times(1)).activateApplication(any(Space.class), eq("newAppId"));
+
   }
 
   @Test
-  public void afterUpgrade() {
+  public void testAfterUpgrade() {
     spaceApplicationMigration.afterUpgrade();
-    SettingValue<?> settingValue = settingService.get(Context.GLOBAL.id("FileExplorerPortlet:Documents"),
-            Scope.APPLICATION.id("FileExplorerPortlet:Documents"),
-            "SpaceApplicationMigrationEnded");
-    assertEquals(true , settingValue.getValue());
-  }
-
-  private void startAdminSession() {
-    HashSet<MembershipEntry> memberships = new HashSet<MembershipEntry>();
-    memberships.add(new MembershipEntry("/platform/users", "*"));
-    memberships.add(new MembershipEntry("/platform/administrators", "*"));
-    Identity root = new Identity("root", memberships);
-    identityRegistry.register(root);
-    ConversationState conversationState = new ConversationState(root);
-    ConversationState.setCurrent(conversationState);
+    verify(settingService, times(1)).set(any(), any(), eq("SpaceApplicationMigrationEnded"), any());
   }
 
 }
