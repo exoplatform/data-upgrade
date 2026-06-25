a2a5da0b3d5f49f5b3a311a311f30c584f8fffc5
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2026-06-25 09:16:30 +0100

    feat: force to remove anonymous permissions for space documents - EXO-87896
    - remove anonymous permissions from documents under /Groups/spaces
    - process nodes in chunks to support large repositories
    - retry upgrade on startup if migration is incomplete
    - verify that no anonymously accessible documents remain after migration

--- data-upgrade-documents-permissions/src/main/java/org/exoplatform/upgrade/AddPublishersPermissionsUpgradePlugin.java
+++ data-upgrade-documents-permissions/src/main/java/org/exoplatform/upgrade/AddPublishersPermissionsUpgradePlugin.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.exoplatform.migration;
+package org.exoplatform.upgrade;
 
 import io.meeds.social.space.constant.SpaceMembershipStatus;
 import jakarta.persistence.TypedQuery;
